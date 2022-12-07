f6da190f8ed603e201f640220e0421a0c53b5330
mateusbrusco <mateusbrusco@gmail.com>
2022-12-07 10:35:31 -0300

    First commit. Project set up.

---
+++ back-end-campeonato-futebol/src/main/java/br/com/campeonato/BackEndCampeonatoFutebolApplication.java
@@ -0,0 +1,13 @@
+package br.com.campeonato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndCampeonatoFutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndCampeonatoFutebolApplication.class, args);
+	}
+
+}
